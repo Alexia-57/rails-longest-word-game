a70eac8df67008b4d984f61af2c0dd138c9a875b
Alexia Sander <sander.alexia@gmail.com>
2023-06-29 22:46:49 +0900

    design review

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,5 +9,8 @@ class GamesController < ApplicationController
 
   def score
     # form will be submitted (with POST) to the score action.
+    # The word can’t be built out of the original grid
+    # The word is valid according to the grid, but is not a valid English word
+    # The word is valid according to the grid and is an English word
   end
 end
